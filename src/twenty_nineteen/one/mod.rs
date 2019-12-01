b63e11686203fa9b9fac1dbf6a21961d4d3da18b
Nick McGuire <nick.mcguire@1password.com>
2019-12-01 10:13:47 -0500

    Updated for part two of day one

--- src/twenty_nineteen/one/mod.rs
+++ src/twenty_nineteen/one/mod.rs
@@ -6,12 +6,31 @@ pub fn cacluate() {
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    let mut total_fuel: i64 = 0;
+    let mut total_fuel_part_one: i64 = 0;
+    let mut total_fuel_part_two: i64 = 0;
 
     for line in contents.lines() {
         let module_mass = line.parse::<i64>().unwrap();
-        let added_fuel = (module_mass / 3) - 2;
-        total_fuel = total_fuel + added_fuel;
+        // Part One
+        let mut added_fuel = calculate_required_fuel(module_mass);
+        total_fuel_part_one = total_fuel_part_one + added_fuel;
+
+        // Part Two
+        while added_fuel > 0 {
+            total_fuel_part_two += added_fuel;
+            added_fuel = calculate_required_fuel(added_fuel);
+        }
     }
-    println!("The entire required fuel is {}", total_fuel);
+    println!(
+        "Part One: The entire required fuel is {}",
+        total_fuel_part_one
+    );
+    println!(
+        "Part Two: The entire required fuel is {}",
+        total_fuel_part_two
+    );
+}
+
+fn calculate_required_fuel(module_mass: i64) -> i64 {
+    return (module_mass / 3) - 2;
 }
