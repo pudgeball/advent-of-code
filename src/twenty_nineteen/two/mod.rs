15135bbf5ba0465366cec3fd4b632fdd0f07161f
Nick McGuire <nick.mcguire@1password.com>
2019-12-03 00:18:00 -0500

    Day 2, Part 2

--- src/twenty_nineteen/two/mod.rs
+++ src/twenty_nineteen/two/mod.rs
@@ -1,13 +1,27 @@
 use std::fs;
+/*
+Turn this into a test
+4138687 => 4138687,12,2,2,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,24,1,19,10,28,2,13,23,140,1,5,27,141,2,6,31,282,1,6,35,284,2,39,9,852,1,5,43,853,1,13,47,858,1,10,51,862,2,55,10,3448,2,10,59,13792,1,9,63,13795,2,67,13,68975,1,71,6,68977,2,6,75,137954,1,5,79,137955,2,83,9,413865,1,6,87,413867,2,91,6,827734,1,95,6,827736,2,99,13,4138680,1,6,103,4138682,1,2,107,4138684,1,111,9,0,99,2,14,0,0
+noun: 66, verb: 35
+*/
 
-fn do_work(input: String) -> String {
+fn turn_vec_to_string(input: Vec<i64>) -> String {
+    let mut returned = "".to_string();
+    for code in input {
+        if returned.is_empty() {
+            returned = format!("{}", code);
+        } else {
+            returned = format!("{},{}", returned, code);
+        }
+    }
+    returned
+}
+
+fn do_work(input: String) -> (i64, String) {
     let mut op_codes = input
         .split(",")
         .map(|n| n.parse::<i64>().unwrap())
         .collect::<Vec<i64>>();
-
-    op_codes[1] = 12;
-    op_codes[2] = 2;
 
     let mut idx = 0;
     loop {
@@ -31,15 +45,9 @@ fn do_work(input: String) -> String {
         idx += 4;
     }
 
-    let mut returned = "".to_string();
-    for code in op_codes {
-        if returned.is_empty() {
-            returned = format!("{}", code);
-        } else {
-            returned = format!("{},{}", returned, code);
-        }
-    }
-    returned
+    let output = op_codes[0].clone();
+    let op_code_string = turn_vec_to_string(op_codes);
+    (output, op_code_string)
 }
 
 pub fn cacluate() {
@@ -48,24 +56,50 @@ pub fn cacluate() {
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    println!("{}", do_work(contents));
+    let mut op_codes = contents
+        .split(",")
+        .map(|n| n.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+
+    op_codes[1] = 12;
+    op_codes[2] = 2;
+
+    let (output, op_codes_string) = do_work(turn_vec_to_string(op_codes.clone()));
+    println!("{} => {}", output, op_codes_string);
+
+    for noun in 0..99 {
+        for verb in 0..99 {
+            op_codes[1] = noun;
+            op_codes[2] = verb;
+
+            let (output, _) = do_work(turn_vec_to_string(op_codes.clone()));
+            if output == 19690720 {
+                println!("noun: {}, verb: {}", noun, verb);
+            }
+        }
+    }
 }
 
 #[test]
 fn test_examples() {
     assert_eq!(
         "3500,9,10,70,2,3,11,0,99,30,40,50",
-        do_work("1,9,10,3,2,3,11,0,99,30,40,50".to_string())
+        do_work("1,9,10,3,2,3,11,0,99,30,40,50".to_string()).1
     );
 
-    assert_eq!("2,0,0,0,99", do_work("1,0,0,0,99".to_string()));
+    assert_eq!("2,0,0,0,99", do_work("1,0,0,0,99".to_string()).1);
 
-    assert_eq!("2,3,0,6,99", do_work("2,3,0,3,99".to_string()));
+    assert_eq!("2,3,0,6,99", do_work("2,3,0,3,99".to_string()).1);
 
-    assert_eq!("2,4,4,5,99,9801", do_work("2,4,4,5,99,0".to_string()));
+    assert_eq!("2,4,4,5,99,9801", do_work("2,4,4,5,99,0".to_string()).1);
 
     assert_eq!(
         "30,1,1,4,2,5,6,0,99",
-        do_work("1,1,1,4,99,5,6,0,99".to_string())
+        do_work("1,1,1,4,99,5,6,0,99".to_string()).1
+    );
+
+    assert_eq!(
+        "4138687,12,2,2,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,24,1,19,10,28,2,13,23,140,1,5,27,141,2,6,31,282,1,6,35,284,2,39,9,852,1,5,43,853,1,13,47,858,1,10,51,862,2,55,10,3448,2,10,59,13792,1,9,63,13795,2,67,13,68975,1,71,6,68977,2,6,75,137954,1,5,79,137955,2,83,9,413865,1,6,87,413867,2,91,6,827734,1,95,6,827736,2,99,13,4138680,1,6,103,4138682,1,2,107,4138684,1,111,9,0,99,2,14,0,0",
+        do_work("1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,19,10,23,2,13,23,27,1,5,27,31,2,6,31,35,1,6,35,39,2,39,9,43,1,5,43,47,1,13,47,51,1,10,51,55,2,55,10,59,2,10,59,63,1,9,63,67,2,67,13,71,1,71,6,75,2,6,75,79,1,5,79,83,2,83,9,87,1,6,87,91,2,91,6,95,1,95,6,99,2,99,13,103,1,6,103,107,1,2,107,111,1,111,9,0,99,2,14,0,0".to_string()).1
     );
 }
