7cfff1c772817bd95f9ea54035b29f4d1e48f560
Nick McGuire <nick.mcguire@1password.com>
2020-12-03 00:28:59 -0500

    aoc 2020 - day 3 - part 1

--- src/aoc2020/mod.rs
+++ src/aoc2020/mod.rs
@@ -2,11 +2,13 @@
 
 mod day1;
 mod day2;
+mod day3;
 
 pub(crate) fn run_day(day: usize) {
     match day {
         1 => day1::run(),
         2 => day2::run(),
+        3 => day3::run(),
         _ => unimplemented!("These puzzles were not completed"),
     }
 }
